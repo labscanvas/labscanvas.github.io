4fca0c1c64d399c0a831a5d0a52bc95d5cad1030
vaishakhanil <vaishakh.anil.360@gmail.com>
2025-01-29 15:20:35 -0500

    Development - Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `canvaslabs`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
