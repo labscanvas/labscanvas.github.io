1f83daf059ad33f712e4f7b0ba782a6060ca0660
vaishakhanil <vaishakh.anil.360@gmail.com>
2025-01-30 09:21:20 -0500

    Deployment - Final Deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import * as React from "react"
-import { Banner } from "../components"
+import "../static/style.css"
+import { Banner, About, Project } from "../components"
 
 const IndexPage = () => {
   return (
     <main>
       <Banner/>
+      <About/>
+      <Project/>
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Canvas Labs | Home Page</title>
